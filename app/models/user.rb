5f10c544a0417b35e7af0bfe60202b632090173f
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-03-21 20:27:01 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :email,           uniqueness: true
+  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :kana_last_name,  presence: true
+  validates :kana_first_name, presence: true
+  validates :birthday,        presence: true
 end
