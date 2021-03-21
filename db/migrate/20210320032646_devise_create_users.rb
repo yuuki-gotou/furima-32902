3ba0dc09f90a957ab938acc9c852c2e582560d42
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-03-21 20:26:26 +0900

    カラムの追加・誤りの訂正
    必要なカラムの追加
    スペルミスやカラム型・カラム名の変更

--- db/migrate/20210320032646_devise_create_users.rb
+++ db/migrate/20210320032646_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nicname,            null: false
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :last_name,          null: false
       t.string :first_name,         null: false
       t.string :kana_last_name,     null: false
       t.string :kana_first_name,    null: false
-      t.string :date,               null: false
+      t.date :birthday,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,14 @@
 ActiveRecord::Schema.define(version: 2021_03_20_032646) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "kana_last_name", null: false
+    t.string "kana_first_name", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
