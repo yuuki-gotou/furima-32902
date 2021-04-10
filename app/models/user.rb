034a000ebe6626afd6c7ecb4832f38f6b249b392
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-04-10 14:24:11 +0900

    バリデーション記述変更
    presence: trueをwith_optionsでまとめて表記する

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,15 @@ class User < ApplicationRecord
 
   VALID_KATAKANA_REGEX = /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ヶー一-龠]+\z/
+  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ヶー一-龠々]+\z/
 
-  validates :nickname,           presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,       format: { with: VALID_ZENKAKU_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力して下さい' }
+    validates :first_name,      format: { with: VALID_ZENKAKU_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力して下さい' }
+    validates :kana_last_name,  format: { with: VALID_KATAKANA_REGEX, message: 'は全角（カタカナ）で入力して下さい' }
+    validates :kana_first_name, format: { with: VALID_KATAKANA_REGEX, message: 'は全角（カタカナ）で入力して下さい' }
+    validates :birthday
+  end
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力して下さい' }
-  validates :last_name,          presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力して下さい' }
-  validates :first_name,         presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力して下さい' }
-  validates :kana_last_name,     presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'は全角（カタカナ）で入力して下さい' }
-  validates :kana_first_name,    presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'は全角（カタカナ）で入力して下さい' }
-  validates :birthday,           presence: true
 end
