525bb129b1f770dadd9f54f826739ca6c304b9c6
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-04-14 21:11:00 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,8 @@ class ItemsController < ApplicationController
   def move_to_index
     redirected_to action: :index unless user_signed_in?
   end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
 
   has_one :purchase_record
   belongs_to :user
+  has_one_attached :image
+  
 end
