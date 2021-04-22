435417d6053e61a88d009010fd59014560e014e6
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-04-22 22:33:40 +0900

    new,createアクションの定義
    ※paramsの許可でdelivery_source_idの記述が二重にあったので削除している。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,19 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def move_to_index
@@ -10,6 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id,
+                                 :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
