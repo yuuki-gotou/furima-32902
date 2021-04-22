c74e487463e3d67776f57963cfab1e4114f8f9c8
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-04-22 21:45:12 +0900

    販売価格の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function () {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+
+    const profit = document.getElementById("profit")
+    profit.innerHTML = Math.floor(inputValue * 0.9);
+  })
+})
