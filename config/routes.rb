6f7f5e818d56529972048be6473e4403ae0ab0d1
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-04-24 11:20:25 +0900

    訂正　
    do endを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   
-  resources :items, only: [:new, :create] do
-  end
+  resources :items, only: [:new, :create]
 end
