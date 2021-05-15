e860fdad65dc1a7b8c926c5b75cf0aadfb8985da
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-05-15 17:18:51 +0900

    Revert "商品購入関連のファイル作成"
    This reverts commit 596053834f4f6d892e7e5cf36a2c7376f1e42004.

--- db/migrate/20210515053214_create_purchase_records.rb
+++
@@ -1,8 +0,0 @@
-class CreatePurchaseRecords < ActiveRecord::Migration[6.0]
-  def change
-    create_table :purchase_records do |t|
-
-      t.timestamps
-    end
-  end
-end
--- spec/requests/purchase_records_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "PurchaseRecords", type: :request do
-
-end
