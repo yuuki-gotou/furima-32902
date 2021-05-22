3e354a7bae7c4503cb8d0a126ba42d98684cd5ae
yuuki-gotou <gotou.pc01@gotounoMacBook-Air.local>
2021-05-22 21:06:48 +0900

    コードレビュー訂正

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,12 +1,12 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :delivery_source_id, :municipality, :house_number, :building_name, :telephone_number,
-                :user_id, :item_id, :purchase_record_id
+                :user_id, :item_id
 
   with_options presence: true do
     validates :municipality, :house_number, :token
     validates :delivery_source_id, numericality: { other_than: 1 }
-    validates :telephone_number, format: { with: /\A\d{11}\z/ }
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
 
     with_options format: { with: /\A\d{3}-\d{4}\z/ } do
       validates :postal_code, length: { is: 8 }
